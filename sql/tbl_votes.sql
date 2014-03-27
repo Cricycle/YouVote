37ecb79d575078f324a76f33f0075c7d36295a80
Alex Anderson <alexanderson@ufl.edu>
2014-03-27 18:08:28 -0400

    Forgot to save

--- sql/tbl_votes.sql
+++ sql/tbl_votes.sql
@@ -4,6 +4,6 @@
 	voteDate	DATE,
 
 	CONSTRAINT pk_one_vote PRIMARY KEY (userID, photoID),
-	CONSTRAINT fk_user FOREIGN KEY (userID) REFERENCES Users(userID),
-	CONSTRAINT fk_photo FOREIGN KEY (photoID) REFERENCES Photos(photoID)
+	CONSTRAINT fk_votes_user FOREIGN KEY (userID) REFERENCES users(userID),
+	CONSTRAINT fk_photo FOREIGN KEY (photoID) REFERENCES photos(photoID)
 );
