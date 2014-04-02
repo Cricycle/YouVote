a3a48817d03b387426cb2996df91a3f5a77cd14d
Meghan King <meghantking67@gmail.com>
2014-04-02 16:27:50 -0400

    Changed primary key of reportedPhotos table

--- sql/tbl_reportedPhotos.sql
+++ sql/tbl_reportedPhotos.sql
@@ -8,7 +8,7 @@ CREATE TABLE reportedPhotos(
 	reportDate		TIMESTAMP,
 
 	—-what would we want the primary key to be… reporter id and the date… We would want users to be able to report a photo more than once, or maybe only once for the same reportType… Right now it is done as below.
-	CONSTRAINT pk_reportedPhotos PRIMARY KEY (reporterID, reportDate),
+	CONSTRAINT pk_reportedPhotos PRIMARY KEY (reporterID, photoID),
 	CONSTRAINT fk_users_reporterID FOREIGN KEY (reporterID) REFERENCES users(userID),
 	CONSTRAINT fk_photos_reportedPhoto FOREIGN KEY (photoID) REFERENCES photos(photoID),
 
