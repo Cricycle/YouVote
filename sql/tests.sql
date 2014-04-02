c2018ca346ee694fab74142f965f4b93b2fa32be
Chelsea Metcalf <swordsandcarrots@gmail.com>
2014-04-02 18:44:55 -0400

    Added tests file.Added tests file.

---
+++ sql/tests.sql
@@ -0,0 +1,14 @@
+--Testing the userPhotos stored procedure function. On the Users page, the user's photos are displayed as a view.
+--Takes in a userID.
+SELECT * FROM userPhotos(2);
+
+SELECT * FROM photos
+WHERE userID = 2;
+
+
+--On the home page, the winner's photos for categories is displayed as a view.
+SELECT * FROM winnerPhotos;
+
+
+--On the home page, below the winner's, recent uploads for photo categories are displayed.
+SELECT * FROM recentUploads;
