850e099f42d5be347602b8b3319c7d9d5fce783b
Chelsea Metcalf <swordsandcarrots@gmail.com>
2014-04-02 18:15:26 -0400

    Added test data

--- sql/storedprocedure-userPhotos.sql
+++ sql/storedprocedure-userPhotos.sql
@@ -1,8 +1,13 @@
 CREATE OR REPLACE FUNCTION userPhotos(pUserID INT)
-RETURNS TABLE(imagePath VARCHAR(250), description VARCHAR(100), uploadDate TIMESTAMP, shortName VARCHAR(25), fullDescription VARCHAR(100)) AS $$
+RETURNS TABLE
+	(imagePath VARCHAR(250), 
+	description VARCHAR(100), 
+	uploadDate TIMESTAMP, 
+	shortName VARCHAR(25), 
+	fullDescription VARCHAR(100)) AS $$
 BEGIN
     RETURN QUERY
-    SELECT imagePath, description, uploadDate, shortname, fulldescription
+    SELECT P.imagePath, P.description, P.uploadDate, PC.shortname, PC.fulldescription
     FROM Photos AS P
     JOIN photoCategories AS PC ON PC.categoryID = P.categoryID
     WHERE userID = pUserID;
